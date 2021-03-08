ab4f5131f58ae8238b41b2dc803a04dc49f218e4
csant510 <james.sant83@gmail.com>
2021-03-07 23:36:33 -0800

    fix parse error

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { mongo } = require("mongoose");
+
 const express = require("express"),
   app = express(),
   errorController = require("./controllers/errorController"),
@@ -6,6 +8,19 @@ const express = require("express"),
   MongoDB = require("mongodb").MongoClient,
   dbURL = "mongodb://localhost:27017",
   dbName = "recipe_db";
+
+  
+MongoDB.connect(dbURL, { useUnifiedTopology: true }, (error, client) =>{
+  if (error) throw error;
+  let db = client.db(dbName);
+  db.collection("contacts")
+  .find()
+  .toArray((error,data) => {
+    if (error) throw error;
+    console.log(data);
+  });
+})
+
 
 
 app.set("port", process.env.PORT || 3000);
